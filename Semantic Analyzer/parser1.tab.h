0706fa46913ddd5eee3c97e36937f2f53f87061c
Poorvab2525 <poorvabhagwat2525@gmail.com>
2024-10-25 18:42:19 +0530

    Added phase3 code

---
+++ Semantic Analyzer/parser1.tab.h
@@ -0,0 +1,103 @@
+
+/* A Bison parser, made by GNU Bison 2.4.1.  */
+
+/* Skeleton interface for Bison's Yacc-like parsers in C
+   
+      Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+   Free Software Foundation, Inc.
+   
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+   
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     IF = 258,
+     INT = 259,
+     CHAR = 260,
+     FLOAT = 261,
+     DOUBLE = 262,
+     LONG = 263,
+     SHORT = 264,
+     SIGNED = 265,
+     UNSIGNED = 266,
+     STRUCT = 267,
+     RETURN = 268,
+     MAIN = 269,
+     VOID = 270,
+     WHILE = 271,
+     FOR = 272,
+     DO = 273,
+     BREAK = 274,
+     CONTINUE = 275,
+     GOTO = 276,
+     ENDIF = 277,
+     SWITCH = 278,
+     CASE = 279,
+     DEFAULT = 280,
+     identifier = 281,
+     array_identifier = 282,
+     integer_constant = 283,
+     string_constant = 284,
+     float_constant = 285,
+     character_constant = 286,
+     ELSE = 287,
+     MOD_EQUAL = 288,
+     DIVIDE_EQUAL = 289,
+     MULTIPLY_EQUAL = 290,
+     SUBTRACT_EQUAL = 291,
+     ADD_EQUAL = 292,
+     OR_OR = 293,
+     AND_AND = 294,
+     NOT_EQUAL = 295,
+     EQUAL = 296,
+     GREAT = 297,
+     GREAT_EQUAL = 298,
+     LESS = 299,
+     LESS_EQUAL = 300,
+     SIZEOF = 301,
+     NOT = 302,
+     DECREMENT = 303,
+     INCREMENT = 304
+   };
+#endif
+
+
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef int YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+extern YYSTYPE yylval;
+
+
---
+++ Semantic Analyzer/test1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int multiply(int a)
+{
+return 2*a;
+}
+
+int main()
+{
+int a=5;
+int b = multiply(a);
+printf("%d",b);
+}
